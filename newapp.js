a92019ab19cd3d514138bcd364eb520ac0033eba
rajat0017 <rajatkalgaonkar07@gamil.com>
2023-07-04 15:15:56 +0530

    bodyparser

--- newapp.js
+++ newapp.js
@@ -4,15 +4,22 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) =>{
-    console.log("middleware");
-    next();
+const bodyparser= require("body-parser");
+app.use(bodyparser.urlencoded());
+
+app.use('/add-product',(req, res, next) =>{
+    
+    res.send('<form action="/product" method="POST" ><input type="text" name="product"><br><br><input type="text" name="size"><br><br>  <button>Add Product</button></form>');
 });
 
-app.use((req, res, next) =>{
-    console.log('next middleware');
-    res.send('<h1>hello ExpressJs</h1>');
+app.post('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req, res, next) =>{
+    
+    res.send('<h1>redirected to home</h1>');
 });
 
-
-app.listen(4000);
+app.listen(3000);
